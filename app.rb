229ea9121a8402327e12912858de3721ee5cfec2
m <32762403+m-codes@users.noreply.github.com>
2022-12-07 15:36:41 +0000

    working listings mvp

--- app.rb
+++ app.rb
@@ -5,6 +5,8 @@ require_relative 'lib/listing_repository'
 require_relative 'lib/listing'
 
 
+DatabaseConnection.connect('makersbnb_test')
+
 class Application < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
@@ -18,12 +20,33 @@ class Application < Sinatra::Base
 
 
   get '/listings' do
-    repo =
-    @all_listings =
+    repo = ListingRepository.new
+    @all_listings = repo.all
     return erb(:listings)
   end
 
   get '/listings/new' do
     return erb(:new_listings)
   end
+
+
+  post '/listings/new' do
+    new_listing = Listing.new
+    new_listing.name = params[:name]
+    new_listing.description = params[:description]
+    new_listing.price = params[:price]
+    new_listing.user_id = params[:user_id]
+    new_listing.available_from = params[:available_from]
+    new_listing.available_to = params[:available_to]
+
+    repo = ListingRepository.new
+    repo.create(new_listing)
+    return redirect ('/listings')
+  end
+
+
+
+
+
+
 end
